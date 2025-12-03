195dcf51d298f54b80f5681a6ff4e88b98a78c1f
FVS <feliven@gmail.com>
2025-12-03 13:51:48 -0300

    criado teste para botõ Login no teste fazerLogin

--- tests/page-object/PaginaLogin.ts
+++ tests/page-object/PaginaLogin.ts
@@ -2,12 +2,16 @@ import { Locator, Page, expect } from '@playwright/test';
 
 export default class PaginaLogin {
   private readonly page: Page;
+  botaoLogin: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.botaoLogin = page.getByTestId('botao-login');
   }
 
   async visitar() {
     await this.page.goto('/');
+    await this.botaoLogin.click();
+    await expect(this.page).toHaveURL('/auth/login');
   }
 }
