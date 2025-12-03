050884013d73bd21421d28f32b8b20625dbd74d2
FVS <feliven@gmail.com>
2025-12-03 13:27:06 -0300

    criado teste fazerLogin e page-object para ele

---
+++ tests/page-object/PaginaLogin.ts
@@ -0,0 +1,13 @@
+import { Locator, Page, expect } from '@playwright/test';
+
+export default class PaginaLogin {
+  private readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async visitar() {
+    await this.page.goto('/');
+  }
+}
