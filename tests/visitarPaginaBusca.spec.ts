bed3dad94bfd5263d8ea744d1c5fdcff4cc6b1e6
FVS <feliven@gmail.com>
2025-12-03 13:12:35 -0300

    atualiza sintaxe dos testes para home e busca

--- tests/visitarPaginaBusca.spec.ts
+++ tests/visitarPaginaBusca.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Página de busca', () => {
   test('Deve visitar a página de busca', async ({ page }) => {
-    await page.goto('http://localhost:4200/busca'); //ação
+    await page.goto('/busca'); //ação
     await expect(page).toHaveTitle('Jornada Milhas'); //asserção
   });
 });
