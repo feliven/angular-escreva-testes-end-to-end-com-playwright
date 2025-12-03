c684a9538dd66f53ae1f9293861570dfd9f91308
FVS <feliven@gmail.com>
2025-12-03 12:06:22 -0300

    cria teste sobre visitar página de busca

--- tests/visitarPaginaInicial.spec.ts
+++ tests/visitarPaginaInicial.spec.ts
@@ -6,3 +6,10 @@ test.describe('Página Inicial', () => {
     await expect(page).toHaveTitle('Jornada Milhas'); //asserção
   });
 });
+
+test.describe('Página de busca', () => {
+  test('Deve visitar a página de busca', async ({ page }) => {
+    await page.goto('http://localhost:4200/busca'); //ação
+    await expect(page).toHaveTitle('Jornada Milhas'); //asserção
+  });
+});
